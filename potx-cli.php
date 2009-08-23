c1c461ec4d197fe0547800739dad11f0afdd2feb
Gábor Hojtsy <gabor@hojtsy.hu>
2009-08-23 10:25:37 +0000

    #556770 by Dave Reid: remove extra whitespace

--- potx-cli.php
+++ potx-cli.php
@@ -20,13 +20,13 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
 // Functions shared with web based interface
 include dirname(__FILE__) .'/potx.inc';
 
-// We need a lot of resources probably, so try to set memory 
+// We need a lot of resources probably, so try to set memory
 // limit higher and set unlimited time for our work.
 $memory_limit = @ini_get('memory_limit');
 if ($memory_limit != '' && (int)$memory_limit < 16) {
   // ini_get returns the original set value, such as "32M",
   // so we check for the int version. Before PHP 5.2, this
-  // limit was less then 16M. 
+  // limit was less then 16M.
   @ini_set('memory_limit', 16777216);
 }
 @set_time_limit(0);
@@ -61,7 +61,7 @@ Possible options:
      Only perform a 'self test'.
  --help
      Display this message.
- 
+
 END;
       return 1;
       break;
