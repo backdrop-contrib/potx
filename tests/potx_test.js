736086d1911b0f055793deefb6b4993c5c5d8526
Gábor Hojtsy <gabor@hojtsy.hu>
2009-09-08 15:06:11 +0000

    #571538 by myself: adding tests to translation template extractor

---
+++ tests/potx_test.js
@@ -0,0 +1,9 @@
+/* $Id$ */
+
+/**
+ * Test Drupal.t and Drupal.formatPlural() invocation.
+ */
+ 
+Drupal.t('Test string in JS');
+Drupal.formatPlural(count, '1 test string in JS', '@count test strings in JS');
+Drupal.t('Another test string in JS', {'test': Drupal.t('Embedded test string in JS')});
