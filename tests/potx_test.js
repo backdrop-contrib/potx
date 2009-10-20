7e402318982a9cac44563ca8c1370523e67ac26d
Gábor Hojtsy <gabor@hojtsy.hu>
2009-10-20 15:49:36 +0000

    #609590 by myself: fix issue with empty strings being extracted from buggy sources

--- tests/potx_test.js
+++ tests/potx_test.js
@@ -7,3 +7,4 @@
 Drupal.t('Test string in JS');
 Drupal.formatPlural(count, '1 test string in JS', '@count test strings in JS');
 Drupal.t('Another test string in JS', {'test': Drupal.t('Embedded test string in JS')});
+Drupal.t('');
