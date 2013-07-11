e6b4c0c18829ae1c79437cf84a676a189cb85277
Gábor Hojtsy <gabor@hojtsy.hu>
2013-07-11 22:26:54 +0200

    Side effect of #1860254: expand test coverage for format plural with context; make format plural matching more concrete

--- tests/potx_test.js
+++ tests/potx_test.js
@@ -2,8 +2,12 @@
 /**
  * Test Drupal.t and Drupal.formatPlural() invocation.
  */
- 
+
 Drupal.t('Test string in JS');
 Drupal.formatPlural(count, '1 test string in JS', '@count test strings in JS');
+
 Drupal.t('Another test string in JS', {'test': Drupal.t('Embedded test string in JS')});
+
 Drupal.t('');
+
+Drupal.t('String with @placeholder value', {'@placeholder': 'placeholder'});
