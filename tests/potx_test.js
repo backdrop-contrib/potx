a27ca90a027bbe229f724792d0df8d29c6a2eae5
Gábor Hojtsy <gabor@hojtsy.hu>
2013-07-12 03:13:37 +0200

    Issue #1860254 by Gábor Hojtsy, make77: Added Support for Drupal 8/7 context extraction for JS translations.

--- tests/potx_test.js
+++ tests/potx_test.js
@@ -6,8 +6,12 @@
 Drupal.t('Test string in JS');
 Drupal.formatPlural(count, '1 test string in JS', '@count test strings in JS');
 
-Drupal.t('Another test string in JS', {'test': Drupal.t('Embedded test string in JS')});
-
 Drupal.t('');
 
 Drupal.t('String with @placeholder value', {'@placeholder': 'placeholder'});
+
+Drupal.t('Test string in JS in test context', {}, {'context': 'Test context'});
+Drupal.t('Test string in JS in context and with @placeholder', {'@placeholder': 'placeholder'}, {'context': 'Test context'});
+
+Drupal.formatPlural(count, '1 test string in JS in test context', '@count test strings in JS in test context', {'@placeholder': 'placeholder', '%value': 12}, {'context': 'Test context'});
+Drupal.formatPlural(count, '1 test string in JS with context and @placeholder', '@count test strings in JS with context and @placeholder', {'@placeholder': 'placeholder', '%value': 12}, {'context': 'Test context'});
